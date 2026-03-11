e6f899c1cf55c4eb49a1643eaa48700b0d441ade
danielmeloni-hrm <daniel.meloni@hrm.group>
2026-03-11 23:03:07 +0000

    asf

--- src/app/(app)/note_board/page.tsx
+++ src/app/(app)/note_board/page.tsx
@@ -225,22 +225,22 @@ export default function SublimeSupabaseEditor() {
       <div className="flex-1 flex flex-col min-w-0">
         <div className="flex-1 min-h-0 p-1">
           <SublimeEmbed
-            socketUrl="https://sublime-bridge-server.onrender.com"
-            userId={userId}
-            bridgeActive={true}
-            onCodeUpdate={({ code, fileName, tabs }) => {
-    console.log("Nuovo codice ricevuto:", code);}}
-            onBridgeStatusChange={handleBridgeStatusChange} // Usa la funzione stabile definita sopra
-            highlightRules={[
-              { name: 'cliente', regex: /@([^\s]+)/ },
-              { name: 'tag', regex: /#([\w\d]+)/ },
-              { name: 'title', regex: /"(.*?)"/ },
-              { name: 'collaudo', regex: /\(coll:\s*([^)]+)\)/ },
-              { name: 'produzione', regex: /\(prod:\s*([^)]+)\)/ },
-              { name: 'percentuale', regex: /\[perc:\s*([^\]]+)\]/ },
-              { name: 'in_lavorazione', regex: /\{ora\}/ },
-            ]}
-          />
+  socketUrl="https://sublime-bridge-server.onrender.com"
+  userId={userId}
+  bridgeActive={true}
+  // MODIFICA QUI: Collega la funzione handleCodeUpdate che analizza i tag
+  onCodeUpdate={handleCodeUpdate} 
+  onBridgeStatusChange={handleBridgeStatusChange}
+  highlightRules={[
+    { name: 'cliente', regex: /@([^\s]+)/ },
+    { name: 'tag', regex: /#([\w\d]+)/ },
+    { name: 'title', regex: /"(.*?)"/ },
+    { name: 'collaudo', regex: /\(coll:\s*([^)]+)\)/ },
+    { name: 'produzione', regex: /\(prod:\s*([^)]+)\)/ },
+    { name: 'percentuale', regex: /\[perc:\s*([^\]]+)\]/ },
+    { name: 'in_lavorazione', regex: /\{ora\}/ },
+  ]}
+/>
         </div>
         <div
           className={`h-7 flex items-center justify-between px-3 text-[9px] font-black uppercase shrink-0 ${
